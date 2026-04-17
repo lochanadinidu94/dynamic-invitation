9449c40fe3a861cfed0ca92790c0a59d70f6e0bd
Dinidu Lochana <dinidu.lochanadissanayake@iag.com.au>
2026-04-17 10:15:21 +1000

    feat: update tf state with s3 storages

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket         = "rinnah-state-storage-tf"
+    key            = "state/terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+  }
+}
+
 provider "aws" {
   region = "ap-southeast-2"
 }
 
-# --- 1. S3 BUCKET FOR STATIC WEBSITE ---
+# --- S3 BUCKET FOR WEBSITE ---
 resource "aws_s3_bucket" "rinnah_bucket" {
   bucket = "rinnah-2026-portal"
 }
 
 resource "aws_s3_bucket_website_configuration" "web_config" {
   bucket = aws_s3_bucket.rinnah_bucket.id
-  index_document {
-    suffix = "index.html"
-  }
+  index_document { suffix = "index.html" }
 }
 
 resource "aws_s3_bucket_policy" "public_read_policy" {
@@ -19,16 +26,13 @@ resource "aws_s3_bucket_policy" "public_read_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Sid       = "PublicReadGetObject"
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.rinnah_bucket.arn}/*"
+      Effect = "Allow", Principal = "*", Action = "s3:GetObject",
+      Resource = "${aws_s3_bucket.rinnah_bucket.arn}/*"
     }]
   })
 }
 
-# --- 2. LAMBDA FUNCTION ---
+# --- LAMBDA FUNCTION ---
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_file = "${path.module}/lambda_function.py"
@@ -44,19 +48,12 @@ resource "aws_lambda_function" "rsvp_lambda" {
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
 
-# --- 3. IAM ROLE FOR LAMBDA ---
+# --- IAM ROLE ---
 resource "aws_iam_role" "lambda_exec_role" {
   name = "rinnah_lambda_execution_role"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
+    Statement = [{ Action = "sts:AssumeRole", Effect = "Allow", Principal = { Service = "lambda.amazonaws.com" } }]
   })
 }
 
@@ -65,21 +62,13 @@ resource "aws_iam_role_policy" "lambda_s3_access" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["s3:GetObject", "s3:PutObject"]
-        Resource = "${aws_s3_bucket.rinnah_bucket.arn}/*"
-      },
-      {
-        Effect   = "Allow"
-        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
-        Resource = "arn:aws:logs:*:*:*"
-      }
+      { Effect = "Allow", Action = ["s3:GetObject", "s3:PutObject"], Resource = "${aws_s3_bucket.rinnah_bucket.arn}/*" },
+      { Effect = "Allow", Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"], Resource = "arn:aws:logs:*:*:*" }
     ]
   })
 }
 
-# --- 4. API GATEWAY (HTTP API) ---
+# --- API GATEWAY ---
 resource "aws_apigatewayv2_api" "rsvp_api" {
   name          = "rinnah-rsvp-api"
   protocol_type = "HTTP"
@@ -90,7 +79,7 @@ resource "aws_apigatewayv2_api" "rsvp_api" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
+resource "aws_apigatewayv2_integration" "lambda_int" {
   api_id           = aws_apigatewayv2_api.rsvp_api.id
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.rsvp_lambda.invoke_arn
@@ -99,16 +88,16 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
 resource "aws_apigatewayv2_route" "rsvp_route" {
   api_id    = aws_apigatewayv2_api.rsvp_api.id
   route_key = "POST /rsvp"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_int.id}"
 }
 
-resource "aws_apigatewayv2_stage" "prod_stage" {
-  api_id      = aws_apigatewayv2_api.rsvp_api.id
-  name        = "$default"
+resource "aws_apigatewayv2_stage" "prod" {
+  api_id = aws_apigatewayv2_api.rsvp_api.id
+  name   = "$default"
   auto_deploy = true
 }
 
-resource "aws_lambda_permission" "allow_api_gateway" {
+resource "aws_lambda_permission" "allow_api" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.rsvp_lambda.function_name
@@ -116,11 +105,7 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   source_arn    = "${aws_apigatewayv2_api.rsvp_api.execution_arn}/*/*"
 }
 
-# --- 5. OUTPUTS ---
-output "website_endpoint" {
-  value = aws_s3_bucket_website_configuration.web_config.website_endpoint
-}
-
+# --- OUTPUTS ---
 output "rsvp_api_endpoint" {
   value = "${aws_apigatewayv2_api.rsvp_api.api_endpoint}/rsvp"
 }
