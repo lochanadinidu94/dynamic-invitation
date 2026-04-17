14b2a93b07d8dcfc06f408f71e96fa0c2773861f
Dinidu Lochana <dinidu.lochanadissanayake@iag.com.au>
2026-04-17 10:43:29 +1000

    feat: fix tf

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ async function submitToLambda() {
 
     try {
         // IMPORTANT: Paste your output 'rsvp_api_endpoint' here
-        const API_URL = "https://ajcgjpzb25.execute-api.ap-southeast-2.amazonaws.com/rsvp";
+        const API_URL = "https://x0p16znd81.execute-api.ap-southeast-2.amazonaws.com/rsvp";
 
         const payload = {
             id: currentGuest.ID,
